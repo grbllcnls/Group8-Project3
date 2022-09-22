d8313facb7a246f4a5e069c2d1c6d33c6c8c5d7f
grbllcnls <gabriellecanlas6@gmail.com>
2022-09-22 22:42:10 +0800

    print ip address + network

--- Project3.py
+++ Project3.py
@@ -5,8 +5,13 @@ def get_ip():
     return response["ip"]
 
 def get_information():
-   
+    ip_address = get_ip()
     response = requests.get(f'https://ipapi.co/{ip_address}/json/').json()
+    information_data = {
+        "ip": ip_address,
+        "network" : response.get("network")
+        
+    }
+    return information_data
 
-
-get_information()
+print (get_information())
