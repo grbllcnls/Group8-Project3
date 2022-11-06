c0c55f1804f446f042b8b6b3c5938a9dec236ea0
kayoring <113662501+kayoring@users.noreply.github.com>
2022-11-06 16:18:05 +0800

    gets

--- Project3.py
+++ Project3.py
@@ -4,6 +4,47 @@ def get_ip():
     response = requests.get('https://api64.ipify.org/?format=json').json()
     return response["ip"]
 
+def get_network():
+    response = requests.get(f'https://ipapi.co/{ip_address}/json/').json()
+    network = {"Network" : response.get("network")}
+    return network
+
+def get_city():
+    ip_address = get_ip()
+    response = requests.get(f'https://ipapi.co/{ip_address}/json/').json()
+    city = {"City" : response.get("city")}
+    return city
+
+def get_version():
+    ip_address = get_ip()
+    response = requests.get(f'https://ipapi.co/{ip_address}/json/').json()
+    version = {"Version" : response.get("version")}
+    return version
+
+def get_region():
+    ip_address = get_ip()
+    response = requests.get(f'https://ipapi.co/{ip_address}/json/').json()
+    region = {"Region" : response.get("region")}
+    return region
+
+def get_countryname():
+    ip_address = get_ip()
+    response = requests.get(f'https://ipapi.co/{ip_address}/json/').json()
+    countryname = {"Country Name" : response.get("country_name")}
+    return countryname
+
+def get_org():
+    ip_address = get_ip()
+    response = requests.get(f'https://ipapi.co/{ip_address}/json/').json()
+    org = {"Org" : response.get("org")}
+    return org
+
+def get_postal():
+    ip_address = get_ip()
+    response = requests.get(f'https://ipapi.co/{ip_address}/json/').json()
+    postal = {"Postal" : response.get("postal")}
+    return postal
+
 def get_information():
     ip_address = get_ip()
     response = requests.get(f'https://ipapi.co/{ip_address}/json/').json()
@@ -20,3 +61,6 @@ def get_information():
     return information_data
 
 print (get_information())
+print (get_postal())
+print (get_org())
+print (get_countryname())
